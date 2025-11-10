e0b9176aaa961ab18ff830f0d4302bf20e3b023b
A-DAVI <cassolidavi603@gmail.com>
2025-11-10 19:57:19 -0300

    Criando Repository do projeto

---
+++ demo/demo/src/main/java/com/prova/demo/Repository/CursoRepository.java
@@ -0,0 +1,10 @@
+package com.prova.demo.Repository;
+
+
+import com.prova.demo.Model.Curso;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CursoRepository extends JpaRepository<Curso, Long> {
+}
