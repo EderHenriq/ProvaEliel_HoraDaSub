02f5be2d9880c78fae369bf81e21a833d54ac653
A-DAVI <cassolidavi603@gmail.com>
2025-11-10 20:36:26 -0300

    Criando Controller

--- demo/demo/src/main/java/com/prova/demo/Controller/AlunoController.java
+++ demo/demo/src/main/java/com/prova/demo/Controller/AlunoController.java
@@ -1,4 +1,36 @@
 package com.prova.demo.Controller;
 
+import com.prova.demo.Model.Aluno;
+import com.prova.demo.Service.AlunoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/api")
 public class AlunoController {
+    @Autowired
+    private AlunoService alunoService;
+
+    @PostMapping()
+    public ResponseEntity<Aluno> CriarALuno(@RequestBody Aluno aluno){
+        Aluno novoAluno = alunoService.criarAluno(aluno);
+    }
+
+    @GetMapping("api/id")
+    public List<Aluno> ListarAluno(@RequestBody Aluno aluno){
+        Aluno  = alunoService.ListarTodos();
+        return ListarAluno();
+    }
+
+    @DeleteMapping
+    public List<Aluno> DeletarAluno<@RequestParam Long id>
+
+
+
+
+
 }
